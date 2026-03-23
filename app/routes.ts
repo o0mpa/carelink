1da15c2d60796be3ef331c333cf258c22f1dba29
o0mpa <youremail@example.com>
2026-03-24 00:05:14 +0200

    aa

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  // Landing Pages
+<<<<<<< HEAD
+  // Landing Pages 
   index("components/landing/home.tsx"),
   route("info", "components/landing/info.tsx"),
   route("contact", "components/landing/contact.tsx"),
@@ -11,8 +12,15 @@ export default [
   route("register/client", "components/information/register-client.tsx"),
   route("register/caregiver", "components/information/register-caregiver.tsx"),
   route("login", "components/information/login.tsx"),
-
-  // Profiles
+  route("forgot-password", "components/information/forgot-password.tsx"),
+  // Profile 
   route("profile/client", "components/profile/client-profile.tsx"),
   route("profile/caregiver", "components/profile/caregiver-profile.tsx"),
+=======
+    index("routes/home.tsx"),
+    route("info", "routes/info.tsx"),
+    route("contact", "routes/contact.tsx"),
+    route("login", "routes/login.tsx"),
+    
+>>>>>>> 2f2434ffc83abfb25a1bd2f35a60139146b90ced
 ] satisfies RouteConfig;
